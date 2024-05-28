ee832b139aeea711b7b77ae4d6fc44e500012c37
Ashirwad Swamy <ashirwad.swamy@excelindia.com>
2024-05-28 18:49:52 +0530

    fixed product fetching

---
+++ home_applience_service_app_back_end/src/main/java/com/excel/homeas/dto/TecnicianLoginDto.java
@@ -0,0 +1,18 @@
+package com.excel.homeas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TecnicianLoginDto {
+	private String email;
+	private String password;
+
+}
